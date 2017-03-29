1e0d807eb89ef343c413ad7996071bcfbb64475f
Presbyter <xingchao0203@hotmail.com>
2017-03-29 14:40:56 +0800

    add publish demo

---
+++ PublishDemo/Model/RabbitReq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class RabbitReq
+    {
+        public RabbitReq()
+        {
+            if (null == Data)
+            {
+                Data = new Dictionary<string, object>();
+            }
+        }
+        public Dictionary<string, object> Data { get; set; }
+    }
+}
---
+++ PublishDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using Model;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Demo.Publish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = "amqp://rabbitmq_admin:123456@192.168.1.25:5672/test"
+            };
+            using (IConnection conn = factory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    string input = string.Empty;
+                    while ("exit" != (input = Console.ReadLine()))
+                    {
+                        RabbitReq request = new RabbitReq();
+                        request.Data.Add("column1", input);
+                        request.Data.Add("column2", "test str 2");
+                        request.Data.Add("column3", new { Account = "xingchao", Password = "123456" });
+                        var bodyBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(request));
+
+                        IBasicProperties prop = new BasicProperties();
+                        prop.ContentType = "application/json";
+                        prop.ContentEncoding = "UTF-8";
+
+                        channel.BasicPublish("publish", "", prop, bodyBytes);
+                    }
+                }
+            }
+        }
+    }
+}
